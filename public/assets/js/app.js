79236c033efa18b99fb6951e5b623fda3eb8cc02
Sale2021 <salediallo61@gmail.com>
2024-03-20 03:31:06 +0000

    add datatable, swwetalert asset and change btn action icon

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,96 +1,150 @@
-'use strict';
+"use strict";
 
 /* ===== Enable Bootstrap Popover (on element  ====== */
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
+var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-toggle="popover"]')
+);
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
+    return new bootstrap.Popover(popoverTriggerEl);
+});
 
 /* ==== Enable Bootstrap Alert ====== */
-var alertList = document.querySelectorAll('.alert')
+var alertList = document.querySelectorAll(".alert");
 alertList.forEach(function (alert) {
-  new bootstrap.Alert(alert)
+    new bootstrap.Alert(alert);
 });
-
 
 /* ===== Responsive Sidepanel ====== */
-const sidePanelToggler = document.getElementById('sidepanel-toggler'); 
-const sidePanel = document.getElementById('app-sidepanel');  
-const sidePanelDrop = document.getElementById('sidepanel-drop'); 
-const sidePanelClose = document.getElementById('sidepanel-close'); 
+const sidePanelToggler = document.getElementById("sidepanel-toggler");
+const sidePanel = document.getElementById("app-sidepanel");
+const sidePanelDrop = document.getElementById("sidepanel-drop");
+const sidePanelClose = document.getElementById("sidepanel-close");
 
-window.addEventListener('load', function(){
-	responsiveSidePanel(); 
+window.addEventListener("load", function () {
+    responsiveSidePanel();
 });
 
-window.addEventListener('resize', function(){
-	responsiveSidePanel(); 
+window.addEventListener("resize", function () {
+    responsiveSidePanel();
 });
-
 
 function responsiveSidePanel() {
     let w = window.innerWidth;
-	if(w >= 1200) {
-	    // if larger 
-	    //console.log('larger');
-		sidePanel.classList.remove('sidepanel-hidden');
-		sidePanel.classList.add('sidepanel-visible');
-		
-	} else {
-	    // if smaller
-	    //console.log('smaller');
-	    sidePanel.classList.remove('sidepanel-visible');
-		sidePanel.classList.add('sidepanel-hidden');
-	}
-};
+    if (w >= 1200) {
+        // if larger
+        //console.log('larger');
+        sidePanel.classList.remove("sidepanel-hidden");
+        sidePanel.classList.add("sidepanel-visible");
+    } else {
+        // if smaller
+        //console.log('smaller');
+        sidePanel.classList.remove("sidepanel-visible");
+        sidePanel.classList.add("sidepanel-hidden");
+    }
+}
 
-sidePanelToggler.addEventListener('click', () => {
-	if (sidePanel.classList.contains('sidepanel-visible')) {
-		console.log('visible');
-		sidePanel.classList.remove('sidepanel-visible');
-		sidePanel.classList.add('sidepanel-hidden');
-		
-	} else {
-		console.log('hidden');
-		sidePanel.classList.remove('sidepanel-hidden');
-		sidePanel.classList.add('sidepanel-visible');
-	}
+sidePanelToggler.addEventListener("click", () => {
+    if (sidePanel.classList.contains("sidepanel-visible")) {
+        console.log("visible");
+        sidePanel.classList.remove("sidepanel-visible");
+        sidePanel.classList.add("sidepanel-hidden");
+    } else {
+        console.log("hidden");
+        sidePanel.classList.remove("sidepanel-hidden");
+        sidePanel.classList.add("sidepanel-visible");
+    }
 });
 
-
-
-sidePanelClose.addEventListener('click', (e) => {
-	e.preventDefault();
-	sidePanelToggler.click();
+sidePanelClose.addEventListener("click", (e) => {
+    e.preventDefault();
+    sidePanelToggler.click();
 });
 
-sidePanelDrop.addEventListener('click', (e) => {
-	sidePanelToggler.click();
+sidePanelDrop.addEventListener("click", (e) => {
+    sidePanelToggler.click();
 });
-
-
 
 /* ====== Mobile search ======= */
-const searchMobileTrigger = document.querySelector('.search-mobile-trigger');
-const searchBox = document.querySelector('.app-search-box');
+const searchMobileTrigger = document.querySelector(".search-mobile-trigger");
+const searchBox = document.querySelector(".app-search-box");
 
-searchMobileTrigger.addEventListener('click', () => {
+searchMobileTrigger.addEventListener("click", () => {
+    searchBox.classList.toggle("is-visible");
 
-	searchBox.classList.toggle('is-visible');
-	
-	let searchMobileTriggerIcon = document.querySelector('.search-mobile-trigger-icon');
-	
-	if(searchMobileTriggerIcon.classList.contains('fa-search')) {
-		searchMobileTriggerIcon.classList.remove('fa-search');
-		searchMobileTriggerIcon.classList.add('fa-times');
-	} else {
-		searchMobileTriggerIcon.classList.remove('fa-times');
-		searchMobileTriggerIcon.classList.add('fa-search');
-	}
-	
-		
-	
+    let searchMobileTriggerIcon = document.querySelector(
+        ".search-mobile-trigger-icon"
+    );
+
+    if (searchMobileTriggerIcon.classList.contains("fa-search")) {
+        searchMobileTriggerIcon.classList.remove("fa-search");
+        searchMobileTriggerIcon.classList.add("fa-times");
+    } else {
+        searchMobileTriggerIcon.classList.remove("fa-times");
+        searchMobileTriggerIcon.classList.add("fa-search");
+    }
 });
 
+function deleteConfirmation(url, text, confirmButtonText, title) {
+    swal.fire({
+        title: title,
+        icon: "question",
+        text: text,
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonText: confirmButtonText,
+        cancelButtonText: "Non, Annuler!",
+        reverseButtons: true,
+    }).then(
+        function (e) {
+            if (e.value === true) {
+                var CSRF_TOKEN = $('meta[name="csrf-token"]').attr("content");
+                $.ajax({
+                    type: "DELETE",
+                    url: url,
+                    data: { _token: CSRF_TOKEN },
+                    dataType: "JSON",
+                    success: function (results) {
+                        if (results.success === true) {
+                            swal.fire("Done!", results.message, "success");
+                            // refresh page after 2 seco nds
+                            setTimeout(function () {
+                                location.reload();
+                            }, 2000);
+                        } else {
+                            swal.fire("Error!", results.message, "error");
+                        }
+                    },
+                });
+            } else {
+                e.dismiss;
+            }
+        },
+        function (dismiss) {
+            return false;
+        }
+    );
+}
 
+// boostrap validation js function
+(() => {
+    "use strict";
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll(".needs-validation");
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form) => {
+        form.addEventListener(
+            "submit",
+            (event) => {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add("was-validated");
+            },
+            false
+        );
+    });
+})();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <meta name="description" content="Portal - Bootstrap 5 Admin Dashboard Template For Developers">
-    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">
     <link rel="shortcut icon" href="favicon.ico">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- FontAwesome JS-->
     <script defer src="{{ asset('assets/plugins/fontawesome/js/all.min.js') }}"></script>
 
@@ -24,7 +22,8 @@
     <header class="app-header fixed-top">
         @include('layouts.topbar')
         @include('layouts.sidebar')
-    </header><!--//app-header-->
+    </header>
+    <!--//app-header-->
 
     <div class="app-wrapper">
 
@@ -33,8 +32,10 @@
                 @yield('content')
 
 
-            </div><!--//container-fluid-->
-        </div><!--//app-content-->
+            </div>
+            <!--//container-fluid-->
+        </div>
+        <!--//app-content-->
 
         {{-- <footer class="app-footer">
             <div class="container text-center py-3">
@@ -44,9 +45,11 @@
                         OUREIBA</a></small>
 
             </div>
-        </footer><!--//app-footer--> --}}
+        </footer>
+        <!--//app-footer--> --}}
 
-    </div><!--//app-wrapper-->
+    </div>
+    <!--//app-wrapper-->
 
 
     <!-- Javascript -->
@@ -59,7 +62,19 @@
 
     <!-- Page Specific JS -->
     <script src="{{ asset('assets/js/app.js') }}"></script>
-
+    <script src="{{ asset('assets/js/sweetalert2@11.js') }}"></script>
+    <script>
+        //         $(document).ready( function () {
+//     $('#myTable').DataTable({
+//         responsive: true,
+//         autoWidth: true,
+//         'order': [[0, 'desc']],
+//         language: {
+//         url: "{{ asset('assets/js/fr-FR.json') }}",
+//         }
+//     });
+// });
+    </script>
 </body>
 
 </html>
